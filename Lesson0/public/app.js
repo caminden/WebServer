10e756b01da24ee484b4af8e60fa7860ca90a9a8
chase minden <caminden@gmail.com>
2021-01-22 21:10:52 -0600

    cloud function call

--- Lesson0/public/app.js
+++ Lesson0/public/app.js
@@ -1,6 +1,21 @@
 const buttonTag = document.getElementById("button-lucky")
+
+const f1 = firebase.functions().httpsCallable('doSomething')
+
 buttonTag.addEventListener("click", buttonClick)
 
-function buttonClick(){
+async function buttonClick(){
     console.log("Button clicked")
+    try{
+        const n1 = Math.floor(Math.random() * 100)
+        const n2 = Math.floor(Math.random() * 100)
+        const result = await f1({n1, n2})
+        const ret = result.data
+        console.log(ret)
+        const message = document.getElementById("message")
+        message.innerHTML = "n1 = " + n1 + ", n2 = " + n2
+        + '<br> ' + "Add: " + ret.add + " Mul " + ret.mul
+    }catch(e){
+
+    }
 }
