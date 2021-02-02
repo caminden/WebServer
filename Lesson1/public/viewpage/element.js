7778db06a2ec658be52185dcc48fcceef5d4bde4
chase minden <caminden@gmail.com>
2021-02-01 23:30:31 -0600

    State transfer

--- Lesson1/public/viewpage/element.js
+++ Lesson1/public/viewpage/element.js
@@ -1,5 +1,7 @@
 //menus
 export const menuSignout = document.getElementById("menu-signout")
+export const menuHome = document.getElementById("menu-home")
+export const menuAbout = document.getElementById("menu-about")
 
 //forms
 export const formSignin = document.getElementById("form-signin")
---
+++ Lesson1/public/viewpage/home_page.js
@@ -0,0 +1,13 @@
+import * as Element from '../viewpage/element.js'
+import * as Routes from '../controller/routes.js'
+
+export function addEventListener(){
+    Element.menuHome.addEventListener("click", () => {
+        history.pushState(null, null, Routes.routePath.HOME)
+        home_page()
+    })
+}
+
+export function home_page(){
+    Element.mainContent.innerHTML = '<h1>Home Page</h1>'
+}
