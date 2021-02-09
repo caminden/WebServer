8be10fbb2e7c922fbee10d3486e699a1129e6a84
chase minden <caminden@gmail.com>
2021-02-09 00:02:29 -0600

    Account signup from welcome page

--- Lesson1/public/controller/firebase_controller.js
+++ Lesson1/public/controller/firebase_controller.js
@@ -85,3 +85,7 @@ export async function searchThreads(keywordsArray) {
   })
   return threadList
 }
+
+export async function signUp(email, password){
+    await firebase.auth().createUserWithEmailAndPassword(email, password)
+}
